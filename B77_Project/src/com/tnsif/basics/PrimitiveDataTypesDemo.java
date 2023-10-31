e674430f6548475797b3e93780d9828baf1a063e
Sagar <Sagar@LAPTOP-R2HUB5EQ>
2023-10-31 15:56:37 +0530

    First commit

--- B77_Project/src/com/tnsif/basics/PrimitiveDataTypesDemo.java
+++ B77_Project/src/com/tnsif/basics/PrimitiveDataTypesDemo.java
@@ -11,22 +11,22 @@ public class PrimitiveDataTypesDemo {
 
 		short shortMax = 32767;
 		short shortMin = -32768;
-		System.out.println("Minshort range of byte is" +
-				shortMin+"Maxshort range of byte is "+shortMax);
+		System.out.println("Minshort range of short is" +
+				shortMin+"Maxshort range of short is "+shortMax);
 		
 		//int - 4bytes
 		int maxInt = 2147483647;
 		int minInt = -2147483648;
-		System.out.println("Minint range of byte is" +
-				minInt+"Maxint range of byte is "+maxInt);
+		System.out.println("Minint range of int is" +
+				minInt+"Maxint range of int is "+maxInt);
 		
 
 		//long - 8bytes
 		long maxLong = 9223372036854775807L;
 		long minLong = -9223372036854775808L;
 		
-		System.out.println("Minlong range of byte is" +
-				minLong+"Maxlong range of byte is "+maxLong);
+		System.out.println("Minlong range of long is" +
+				minLong+"Maxlong range of long is "+maxLong);
 		
 		float f=3234.141243278345f;
 		double d=3456.14124512345678902345678914;
